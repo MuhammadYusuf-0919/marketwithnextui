71e59cf920268276520941d371f9e1a61976db86
Zoirjon <zoirjonabduvohidov0@gmail.com>
2023-11-27 15:34:25 +0500

    ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 'use client'
-import Header from "@/components/Navbar";
 import { useSelector } from "react-redux";
 
 export default function Home() {
   const theme = useSelector((state: any) => state.theme.mode)
   return (
-    <div>
-      <Header />
-      <h1>Qalesan</h1>
-    </div>
+    <h1>hi</h1>
   )
 }
